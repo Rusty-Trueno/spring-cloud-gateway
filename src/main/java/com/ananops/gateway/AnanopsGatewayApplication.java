ba3214966968f8d349c8df6c390f59f07d4efec9
Rusty-Trueno <1076061276@qq.com>
2020-03-17 17:17:43 +0800

    gateeway架子初建

---
+++ src/main/java/com/ananops/gateway/AnanopsGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.ananops.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by rongshuai on 2020/3/16 17:24
+ */
+@SpringBootApplication
+public class AnanopsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnanopsGatewayApplication.class,args);
+    }
+}
